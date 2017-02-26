81e08db93cee5ec540881e3c355a1f31bfaf2271
Oscar Davila <odavilar@gmail.com>
2017-02-26 10:22:08 -0600

    Working using formview

--- mynetflix/views.py
+++ mynetflix/views.py
@@ -6,7 +6,7 @@ from django.views import generic
 from .models import Movie, Actor, Genre, Director, Award
 from .forms import SearchForm
 
-class IndexView(generic.ListView):
+class IndexView(generic.DetailView):
     template_name = 'mynetflix/index.html'
     model = Movie
     context_object_name = 'movie_list'
@@ -15,11 +15,25 @@ class IndexView(generic.ListView):
         """Return movies ordered by title."""
         return Movie.objects.order_by('-pub_date')[:5]
 
-    def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
-        context['form'] = SearchForm
-        return context
-
 class DetailView(generic.DetailView):
     model = Movie
     template_name = 'mynetflix/detail.html'
+
+class FormView(generic.FormView):
+    template_name = 'mynetflix/index.html'
+    form_class = SearchForm
+    success_url = '/thanks/'
+
+    def form_valid(self, form):
+        # This method is called when valid form data has been POSTed.
+        # It should return an HttpResponse.
+        return super(SearchForm, self).form_valid(form)
+
+    def get_queryset(self):
+        """Return movies ordered by title."""
+        return Movie.objects.order_by('-pub_date')[:5]
+
+    def get_context_data(self, **kwargs):
+        context = super(FormView, self).get_context_data(**kwargs)
+        context['movie_list'] = Movie.objects.order_by('-pub_date')[:5]
+        return context
